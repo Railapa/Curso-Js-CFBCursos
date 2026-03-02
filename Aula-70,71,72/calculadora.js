d8bdfd68c405c51fe6ddb64c615e4e1eafa517ec
Railapa <rai100304@gmail.com>
2026-03-02 20:10:14 -0300

    Curso JS

--- Aula-70,71,72/calculadora.js
+++ Aula-70,71,72/calculadora.js
@@ -4,19 +4,56 @@ const teclaRes = document.querySelector('.res')
 const display = document.querySelector('.display')
 const ton = document.querySelector('#ton')
 const tlimpar = document.querySelector('#tlimpar')
+const tigual = document.querySelector('#tigual')
+
+let sinal = false
+let decimal = false
 
 teclasNum.map((el) => {
     el.addEventListener('click', (evt) => {
-        display.innerHTML += evt.target.innerHTML
+        sinal = false
+        if (evt.target.innerHTML == ',') {
+            if (!decimal) {
+                decimal = true
+                if(display.innerHTML == '0'){
+                    display.innerHTML = ''
+
+                } else{
+                    display.innerHTML += evt.target.innerHTML
+                }
+            }
+        } else {
+            display.innerHTML += evt.target.innerHTML
+        }
     })
 })
 
 teclasOp.map((el) => {
     el.addEventListener('click', (evt) => {
-        display.innerHTML += evt.target.innerHTML
+        if (!sinal) {
+            sinal = true
+            if (display.innerHTML == '0') {
+                display.innerHTML = ''
+            }
+            if (evt.target.innerHTML == 'x') {
+                display.innerHTML += '*'
+            } else {
+                display.innerHTML += evt.target.innerHTML
+            }
+
+        }
     })
 })
 
 tlimpar.addEventListener('click', () => {
+    sinal = false
+    decimal = false
     display.innerHTML = '0'
+})
+
+tigual.addEventListener('click', () => {
+    sinal = false
+    decimal = false
+    const res = eval(display.innerHTML)
+    display.innerHTML = res
 })
