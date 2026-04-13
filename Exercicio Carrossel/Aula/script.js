5f4d41c3944abe3718d2dc03ca6ffd56c37583e7
Railapa <rai100304@gmail.com>
2026-04-13 18:39:45 -0300

    Curso JS

--- Exercicio Carrossel/Aula/script.js
+++ Exercicio Carrossel/Aula/script.js
@@ -1,13 +1,34 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
-  spaceBetween: 4,
+  // Optional parameters
+  direction: 'horizontal',
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
+
+  // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+   breakpoints: {
+    // when window width is >= 320px
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
   }
 });
